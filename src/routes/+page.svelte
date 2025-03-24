0bb087fc85de7c53a616ce1c19d9e1e8c9807cdb
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2025-03-24 21:42:27 +0100

    feat: :tada: Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import '../style/reset.css';
+	import '../style/font.css';
+	import '../style/theme.css';
+	import '../style/app.css';
+</script>
+
+<div class="app-mount">
+	<h1>What a page!</h1>
+	<p>A very cool page indeed.</p>
+	<p>There will be stuff here eventually, but for now, have this cat! 🐈</p>
+	<p>Page generated on: <span>{new Date().toLocaleString()}</span></p>
+</div>
+
+<style>
+	.app-mount {
+		position: absolute;
+		left: 0;
+		top: 0;
+
+		width: 100dvw;
+		height: 100dvh;
+
+		padding: 16px;
+
+		background-color: var(--bg-dark);
+	}
+
+	span {
+		color: var(--text-light);
+	}
+</style>
